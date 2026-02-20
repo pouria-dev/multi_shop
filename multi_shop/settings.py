84b7a409e14d9d9c8555bc9d611ed8291b30ec3c
pouria-dev <pouriahajipour06@gmail.com>
2026-02-20 12:25:14 +0330

    Add accounts app to INSTALLED_APPS in settings.py

--- multi_shop/settings.py
+++ multi_shop/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home.apps.HomeConfig'
+    'home.apps.HomeConfig',
+    'accounts.apps.AccountsConfig',
     
 ]
 
