44b6647680ddd64db2a6b209741127cb40d2d7ce
pouria-dev <pouriahajipour06@gmail.com>
2026-02-21 13:47:52 +0330

    Add login/register forms and login view for user panel

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from django.contrib.auth import authenticate , login
+from .form import Login_Form , Register_Form
 
-# Create your views here.
+
+
+class Login_View(FormView):
+    template_name = None
+    form_class = Login_Form
+    success_url = '/'
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+
+        user = authenticate(username=username , password=password)
+        if user is not None:
+            login(self.request , user)
+
+        return super().form_valid(form)
+    
